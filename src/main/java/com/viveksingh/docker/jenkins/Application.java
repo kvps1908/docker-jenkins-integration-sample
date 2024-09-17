45b7c93e5c434590fa2b7d18bd7cffe506856fb7
Vivek Singj <kunwarvivekpratapsingh191208@gmail.com>
2024-09-17 17:19:30 +0530

    - added a rest point

--- src/main/java/com/viveksingh/docker/jenkins/Application.java
+++ src/main/java/com/viveksingh/docker/jenkins/Application.java
@@ -2,10 +2,17 @@ package com.viveksingh.docker.jenkins;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Application {
 
+	@GetMapping("/test")
+	public String runImage(){
+		return "Docker Image Created and Running";
+	}
 	public static void main(String[] args) {
 		System.out.println("Hello");
 		SpringApplication.run(Application.class, args);
